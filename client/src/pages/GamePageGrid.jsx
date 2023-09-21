9a1d36ad66552b7a43f69ec23d9051eb36f12abe
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-21 14:46:40 +0100

    Add paper to remove matrix gif distraction while playing

--- client/src/pages/GamePageGrid.jsx
+++ client/src/pages/GamePageGrid.jsx
@@ -99,99 +99,109 @@ const GamePageGrid = ({ players, room }) => {
   };
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "column" }}>
-      <Box
-        sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}
-      >
-        <Paper
-          elevation={3}
+    <Paper>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <Box
           sx={{
-            minWidth: "25vw",
-            minHeight: "5vh",
-            maxHeight: "auto",
-            margin: "2em",
-            padding: "1em",
-            textAlign: "center",
-            variant: "h3",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
           }}
         >
-          <Timer
-            timer={timer}
-            setTimer={setTimer}
-            playerReady={playerReady}
-            setPlayerReady={setPlayerReady}
-            sx={{ maxHeight: "25px" }}
-          />
-        </Paper>
-        <Paper
-          elevation={3}
-          sx={{
-            minWidth: "25vw",
-            minHeight: "5vh",
-            maxHeight: "auto",
-            margin: "2em",
-            padding: "1em",
-            textAlign: "center",
-            variant: "h3",
-          }}
-        >
-          <Typography sx={{ maxHeight: "25px" }}>
-            Game Room ID: {room}
-          </Typography>
-        </Paper>
-      </Box>
+          <Paper
+            elevation={3}
+            sx={{
+              minWidth: "25vw",
+              minHeight: "5vh",
+              maxHeight: "auto",
+              margin: "2em",
+              padding: "1em",
+              textAlign: "center",
+              variant: "h3",
+            }}
+          >
+            <Timer
+              timer={timer}
+              setTimer={setTimer}
+              playerReady={playerReady}
+              setPlayerReady={setPlayerReady}
+              sx={{ maxHeight: "25px" }}
+            />
+          </Paper>
+          <Paper
+            elevation={3}
+            sx={{
+              minWidth: "25vw",
+              minHeight: "5vh",
+              maxHeight: "auto",
+              margin: "2em",
+              padding: "1em",
+              textAlign: "center",
+              variant: "h3",
+            }}
+          >
+            <Typography sx={{ maxHeight: "25px" }}>
+              Game Room ID: {room}
+            </Typography>
+          </Paper>
+        </Box>
 
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} order={{ xs: 3, md: 1 }} md={3}>
-            <Item sx={{ overflow: "auto" }}>
-              <PlayerList players={players} />
-            </Item>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} order={{ xs: 3, md: 1 }} md={3}>
+              <Item sx={{ overflow: "auto" }}>
+                <PlayerList players={players} />
+              </Item>
+            </Grid>
+
+            <Grid item xs={12} order={{ xs: 1, md: 2 }} md={6}>
+              <Item>
+                <PlayBox />
+              </Item>
+            </Grid>
+
+            <Grid item xs={12} order={{ xs: 2, md: 3 }} md={3}>
+              <Item>
+                <Typography variant="h4">Winners List</Typography>
+              </Item>
+            </Grid>
           </Grid>
+        </Box>
 
-          <Grid item xs={12} order={{ xs: 1, md: 2 }} md={6}>
-            <Item>
-              <PlayBox />
-            </Item>
-          </Grid>
-
-          <Grid item xs={12} order={{ xs: 2, md: 3 }} md={3}>
-            <Item>
-              <Typography variant="h4">Winners List</Typography>
-            </Item>
-          </Grid>
-        </Grid>
-      </Box>
-
-      <Box
-        sx={{ display: "flex", flexDirection: "row", justifyContent: "center" }}
-      >
-        <Paper
-          elevation={3}
+        <Box
           sx={{
-            minWidth: "25vw",
-            minHeight: "5vh",
-            margin: "2em",
-            padding: "1em",
-            textAlign: "center",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
           }}
         >
-          <Typography variant="span">Player Controls</Typography>
-        </Paper>
-        <Paper
-          elevation={3}
-          sx={{
-            minWidth: "25vw",
-            minHeight: "5vh",
-            margin: "2em",
-            padding: "1em",
-            textAlign: "center",
-          }}
-        >
-          <Typography variant="span">Chat Box</Typography>
-        </Paper>
+          <Paper
+            elevation={3}
+            sx={{
+              minWidth: "25vw",
+              minHeight: "5vh",
+              margin: "2em",
+              padding: "1em",
+              textAlign: "center",
+            }}
+          >
+            <Typography variant="span">Player Controls</Typography>
+          </Paper>
+          <Paper
+            elevation={3}
+            sx={{
+              minWidth: "25vw",
+              minHeight: "5vh",
+              margin: "2em",
+              padding: "1em",
+              textAlign: "center",
+            }}
+          >
+            <Typography variant="span">Chat Box</Typography>
+          </Paper>
+        </Box>
       </Box>
-    </Box>
+    </Paper>
   );
 };
 
