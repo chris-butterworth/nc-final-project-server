40585c1c94a1b26fe189c22f4bd84fa37715b07a
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-21 16:38:59 +0100

    wrap tutorial in Paper for styling

--- client/src/pages/TutorialPage.jsx
+++ client/src/pages/TutorialPage.jsx
@@ -1,18 +1,20 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Paper } from "@mui/material";
 
 const TutorialPage = () => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-        padding: "2em",
-      }}
-    >
-      <Typography variant="h3">The Tutorial</Typography>
-      <Typography variant="span">blah blah blah blah</Typography>
-    </Box>
+    <Paper sx={{ minWidth: "50%", margin: "10px auto", width: "fit-content" }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          padding: "2em",
+        }}
+      >
+        <Typography variant="h3">The Tutorial</Typography>
+        <Typography variant="span">blah blah blah blah</Typography>
+      </Box>
+    </Paper>
   );
 };
 
