cd83d17688572f212485e27e366ec1183c2dd3b0
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-22 14:18:04 +0100

    Add Links to other Tutorial Pages

--- client/src/pages/TutorialPage.jsx
+++ client/src/pages/TutorialPage.jsx
@@ -1,4 +1,5 @@
-import { Box, Typography, Paper } from "@mui/material";
+import { Box, Typography, Paper, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const TutorialPage = () => {
   return (
@@ -13,6 +14,16 @@ const TutorialPage = () => {
       >
         <Typography variant="h3">The Tutorial</Typography>
         <Typography variant="span">blah blah blah blah</Typography>
+        <Link to="/">
+          <Button>Home Page</Button>
+        </Link>
+
+        <Link to="/tutorial/single">
+          <Button>Single Player Tutorial</Button>
+        </Link>
+        <Link to="/tutorial/multi">
+          <Button>Multi-Player Tutorial</Button>
+        </Link>
       </Box>
     </Paper>
   );
