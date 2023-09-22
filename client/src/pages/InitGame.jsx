7f7e05b57653c39d7d8d96e008199375801defc1
Lex Smith <lex.smith@live.com>
2023-09-22 16:34:40 +0100

    implemented grid on initGame page

--- client/src/pages/InitGame.jsx
+++ client/src/pages/InitGame.jsx
@@ -34,92 +34,100 @@ const InitGame = ({ numberofPlayers, room, setRoom, setPlayers }) => {
           alignItems: "center",
         }}
       >
-        <Box sx={{ display: "flex", flexDirection: "row" }}>
-          <Link
-            to={`/`}
-            style={{ textDecoration: "none" }}
-            onClick={() => {
-              socket.emit("createSinglePlayerRoom", (room) => {
-                setRoomAndPlayers(room.roomId, room.players);
-              });
-            }}
-          >
-            <Paper
-              sx={{
-                minWidth: "25vw",
-                minHeight: "30vh",
-                margin: "2em",
-                padding: "1em",
-                textAlign: "center",
-              }}
-            >
-              <PersonIcon fontSize="large" color="primary" />
-              <Typography variant="h3">Single Player</Typography>
-            </Paper>
-          </Link>
-          <Link
-            to={`/`}
-            style={{ textDecoration: "none" }}
-            onClick={() => {
-              socket.emit("createMultiPlayerRoom", (room) => {
-                setRoomAndPlayers(room.roomId, room.players);
-              });
-            }}
-          >
-            <Paper
-              sx={{
-                minWidth: "25vw",
-                minHeight: "30vh",
-                margin: "2em",
-                padding: "1em",
-                textAlign: "center",
-              }}
-            >
-              <GroupIcon fontSize="large" color="primary" />
-              <Typography variant="h3">New Multi-Player</Typography>
-            </Paper>
-          </Link>
-
-          <Paper
-            sx={{
-              minWidth: "25vw",
-              minHeight: "30vh",
-              margin: "2em",
-              padding: "1em",
-              textAlign: "center",
-            }}
-          >
-            <GroupIcon fontSize="large" color="primary" />
-            <Typography variant="h3">Join game</Typography>
-
-            <FormControl>
-              <InputLabel htmlFor="roomcode">Paste room code</InputLabel>
-              <Input
-                id="roomcode"
-                value={roomCodeInput}
-                onChange={(e) => {
-                  setRoomCodeInput(e.target.value);
-                }}
-              />
-              <Button
-                onClick={(e) => {
-                  e.preventDefault();
-                  socket.emit(
-                    "joinMultiPlayerRoom",
-                    roomCodeInput,
-                    (response) => {
-                      console.log(response);
-                      if (response.error)
-                        return setJoinRoomError(response.message);
-                      setRoomAndPlayers(response.roomId, response.players);
-                    }
-                  );
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={4}>
+              <Link
+                to={`/`}
+                style={{ textDecoration: "none" }}
+                onClick={() => {
+                  socket.emit("createSinglePlayerRoom", (room) => {
+                    setRoomAndPlayers(room.roomId, room.players);
+                  });
                 }}
               >
-                Join Room
-              </Button>
-            </FormControl>
-          </Paper>
+                <Paper
+                  sx={{
+                    minWidth: "25vw",
+                    minHeight: "30vh",
+                    margin: "2em",
+                    padding: "1em",
+                    textAlign: "center",
+                  }}
+                >
+                  <PersonIcon fontSize="large" color="primary" />
+                  <Typography variant="h3">Single Player</Typography>
+                </Paper>
+              </Link>
+            </Grid>
+
+            <Grid item xs={12} md={4}>
+              <Link
+                to={`/`}
+                style={{ textDecoration: "none" }}
+                onClick={() => {
+                  socket.emit("createMultiPlayerRoom", (room) => {
+                    setRoomAndPlayers(room.roomId, room.players);
+                  });
+                }}
+              >
+                <Paper
+                  sx={{
+                    minWidth: "25vw",
+                    minHeight: "30vh",
+                    margin: "2em",
+                    padding: "1em",
+                    textAlign: "center",
+                  }}
+                >
+                  <GroupIcon fontSize="large" color="primary" />
+                  <Typography variant="h3">New Multi-Player</Typography>
+                </Paper>
+              </Link>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Paper
+                sx={{
+                  minWidth: "25vw",
+                  minHeight: "30vh",
+                  margin: "2em",
+                  padding: "1em",
+                  textAlign: "center",
+                }}
+              >
+                <GroupIcon fontSize="large" color="primary" />
+                <Typography variant="h3">Join game</Typography>
+
+                <FormControl>
+                  <InputLabel htmlFor="roomcode">Paste room code</InputLabel>
+                  <Input
+                    id="roomcode"
+                    value={roomCodeInput}
+                    onChange={(e) => {
+                      setRoomCodeInput(e.target.value);
+                    }}
+                  />
+                  <Button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      socket.emit(
+                        "joinMultiPlayerRoom",
+                        roomCodeInput,
+                        (response) => {
+                          console.log(response);
+                          if (response.error)
+                            return setJoinRoomError(response.message);
+                          setRoomAndPlayers(response.roomId, response.players);
+                        }
+                      );
+                    }}
+                  >
+                    Join Room
+                  </Button>
+                </FormControl>
+              </Paper>
+            </Grid>
+          </Grid>
         </Box>
         <Link to={`/tutorial`} style={{ textDecoration: "none" }}>
           <Paper
@@ -132,9 +140,7 @@ const InitGame = ({ numberofPlayers, room, setRoom, setPlayers }) => {
             }}
           >
             <SchoolIcon fontSize="large" color="primary" />
-            <Typography variant="h3">
-              Tutorial
-            </Typography>
+            <Typography variant="h3">Tutorial</Typography>
           </Paper>
         </Link>
       </Box>
