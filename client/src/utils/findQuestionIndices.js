bc691223a8633325e90db40b787ec4ebf98e5184
“cCody34git config --global user.name cCody34git config -- global user.name cCody34 <charlotte.cody23@gmail.com>
2023-09-27 11:23:06 +0100

    edit findQuestionIndices for letters already used

--- client/src/utils/findQuestionIndices.js
+++ client/src/utils/findQuestionIndices.js
@@ -1,6 +1,12 @@
 import makeAnagramButtons from "./makeAnagramButtons";
-
-const findQuestionIndices = (letter, anagramWords, disabledButtons) => {
+const findQuestionIndices = (
+  letter,
+  anagramWords,
+  disabledButtons,
+  formattedAnswerArray,
+  answerWordToReplace,
+  answerLetterToReplace,
+) => {
   // Format anagramLetters to include their word and letter index
   const anagramButtons = makeAnagramButtons(anagramWords);
   // Get a list of potential question/anagram buttons, based on them having the correct letter
@@ -29,7 +35,33 @@ const findQuestionIndices = (letter, anagramWords, disabledButtons) => {
     // If none of the disabled buttons have that word and letter index
     if (filterResult.length === 0) {
       // Then return this word and letter index, as we can disable this button
-      return [potentialButtons[i].wordIndex, potentialButtons[i].letterIndex];
+      return [
+        potentialButtons[i].wordIndex,
+        potentialButtons[i].letterIndex,
+        false,
+      ];
+    }
+  }
+  // If all buttons have been used it will get to this point of the code
+  for (let k = answerWordToReplace; k < formattedAnswerArray.length; k++) {
+    if (k === answerLetterToReplace) {
+      for (let l = 0; l < formattedAnswerArray[k].length; l++) {
+        if (formattedAnswerArray[k][l] === letter) {
+          // Invoke a remove from formattedAnswerArray function
+          return [k, l, { removeWord: k, removeLetter: l }];
+        }
+      }
+    } else {
+      for (
+        let l = answerLetterToReplace + 1;
+        l < formattedAnswerArray[k].length;
+        l++
+      ) {
+        if (formattedAnswerArray[k][l] === letter) {
+          // Invoke a remove from formattedAnswerArray function
+          return [k, l, { removeWord: k, removeLetter: l }];
+        }
+      }
     }
   }
 };
